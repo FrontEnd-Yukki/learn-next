8b86a0a0ee0bdba86c48a0842aec442b7c4f3098
Yukki <frontend.yukki+github@gmail.com>
2021-08-08 13:15:03 +0900

    Dateコンポーネントをページに組み込み

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
+import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
@@ -24,6 +25,7 @@ export default function Post({ postData }) {
     <Head>
       <title>{postData.title}</title>
     </Head>
+    <Date dateString={postData.date} />
     {postData.title}
     <br />
     {postData.id}
